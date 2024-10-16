4ef7f86a31de9fb6eac25e16bf162ebe902af033
Dennismaina <mumbidenis@gmail.com>
2024-10-16 08:13:58 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px"
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const RightNumbers = dodger.style.right.replace("px", "");
+    const Right = parseInt(RightNumbers, 10);
+  
+    if (Right > 0) {
+      dodger.style.Right = `${Right + 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
